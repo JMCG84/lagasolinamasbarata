ea1e7fb6115f9e16cd13a2e25cca69eadca7f1f0
Jose M Campos <camposgarciajosemanuel@gmail.com>
2026-03-08 11:36:19 +0100

    "Add Vercel Analytics"

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
-import router from './router';
+import { createApp } from "vue";
+import App from "./App.vue";
+import { inject } from "@vercel/analytics"; // 1. Importas la función
 
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+inject(); // 2. La ejecutas al arrancar la app
+
+createApp(App).mount("#app");
